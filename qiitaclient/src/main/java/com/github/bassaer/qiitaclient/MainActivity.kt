de3e6ca0c307250bf2e5f153cf55f62f99ac3ba8
Tsubasa Nakayama <app.nakayama@gmail.com>
2017-09-15 22:22:14 +0900

    Add search function

--- qiitaclient/src/main/java/com/github/bassaer/qiitaclient/MainActivity.kt
+++ qiitaclient/src/main/java/com/github/bassaer/qiitaclient/MainActivity.kt
@@ -2,9 +2,20 @@ package com.github.bassaer.qiitaclient
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.Button
+import android.widget.EditText
 import android.widget.ListView
 import com.github.bassaer.qiitaclient.model.Article
+import com.github.bassaer.qiitaclient.model.ArticleClient
 import com.github.bassaer.qiitaclient.model.User
+import com.google.gson.FieldNamingPolicy
+import com.google.gson.GsonBuilder
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
+
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,14 +24,37 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val listAdapter = ArticleListAdapter(applicationContext)
-        listAdapter.articles = listOf(
-                createArticle("Kotlin Book", "Kotlin"),
-                createArticle("Java Book", "Java"))
         val listView: ListView = findViewById(R.id.list_view) as ListView
         listView.adapter = listAdapter
         listView.setOnItemClickListener { _, _, position, _ ->
             val article = listAdapter.articles[position]
             ArticleActivity.intnet(this, article).let { startActivity(it) }
+        }
+
+        val gson = GsonBuilder()
+                .setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES)
+                .create()
+        val retrofit = Retrofit.Builder()
+                .baseUrl("http://qiita.com")
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                .build()
+        val articleClient = retrofit.create(ArticleClient::class.java)
+
+        val queryEditText = findViewById(R.id.query_edit_text) as EditText
+        val searchButton = findViewById(R.id.search_button) as Button
+
+        searchButton.setOnClickListener {
+            articleClient.search(queryEditText.text.toString())
+                    .subscribeOn(Schedulers.io())
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .subscribe ({
+                        queryEditText.text.clear()
+                        listAdapter.articles = it
+                        listAdapter.notifyDataSetChanged()
+                    }, {
+                      toast("Error : $it")
+                    })
         }
 
     }
--- qiitaclient/src/main/java/com/github/bassaer/qiitaclient/extensions.kt
+++ qiitaclient/src/main/java/com/github/bassaer/qiitaclient/extensions.kt
@@ -1,7 +1,9 @@
 package com.github.bassaer.qiitaclient
 
+import android.content.Context
 import android.support.annotation.IdRes
 import android.view.View
+import android.widget.Toast
 
 /**
  * Extensions
@@ -9,4 +11,8 @@ import android.view.View
  */
 fun <T : View> View.bindView(@IdRes id: Int): Lazy<T> = lazy {
     findViewById(id) as T
+}
+
+fun Context.toast(message: String, duration: Int = Toast.LENGTH_SHORT) {
+    Toast.makeText(this, message, duration).show()
 }
