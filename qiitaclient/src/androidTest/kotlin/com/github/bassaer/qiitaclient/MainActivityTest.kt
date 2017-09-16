b7422eda69ff549381507c12cc01b9042347b08f
Tsubasa Nakayama <app.nakayama@gmail.com>
2017-09-16 15:24:34 +0900

    Add UI tests

--- qiitaclient/src/androidTest/kotlin/com/github/bassaer/qiitaclient/MainActivityTest.kt
+++ qiitaclient/src/androidTest/kotlin/com/github/bassaer/qiitaclient/MainActivityTest.kt
@@ -1,17 +1,22 @@
 package com.github.bassaer.qiitaclient
 
 import android.support.test.espresso.Espresso.onView
+import android.support.test.espresso.action.ViewActions.click
+import android.support.test.espresso.action.ViewActions.typeText
 import android.support.test.espresso.assertion.ViewAssertions.matches
 import android.support.test.espresso.matcher.ViewMatchers.isDisplayed
 import android.support.test.espresso.matcher.ViewMatchers.withId
 import android.support.test.rule.ActivityTestRule
 import android.support.test.runner.AndroidJUnit4
 import android.view.View
+import com.github.bassaer.qiitaclient.model.ArticleClient
+import io.reactivex.Observable
 import org.hamcrest.Matcher
 import org.hamcrest.Matchers.not
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.mockito.Mockito.*
 
 /**
  * Created by nakayama on 2017/09/16.
@@ -29,6 +34,18 @@ class MainActivityTest {
         onView(withId(R.id.query_edit_text)).check(matches(isDisplayed()))
         onView(withId(R.id.search_button)).check(matches(isDisplayed()))
         onView(withId(R.id.progress_bar)).check(matches(isNotDisplayed()))
+    }
+
+    @Test
+    fun send_query_when_search_button_was_tapped() {
+        val articleClient = mock(ArticleClient::class.java).apply {
+            `when`(search("user:bassaer")).thenReturn(Observable.just(listOf()))
+        }
+        activityTestRule.activity.articleClient = articleClient
+
+        onView(withId(R.id.query_edit_text)).perform(typeText("user:bassaer"))
+        onView(withId(R.id.search_button)).perform(click())
+        verify(articleClient).search("user:bassaer")
     }
 
     fun isNotDisplayed(): Matcher<View> = not(isDisplayed())
--- qiitaclient/src/main/kotlin/com/github/bassaer/qiitaclient/MainActivity.kt
+++ qiitaclient/src/main/kotlin/com/github/bassaer/qiitaclient/MainActivity.kt
@@ -1,11 +1,10 @@
 package com.github.bassaer.qiitaclient
 
+import android.content.Context
 import android.os.Bundle
 import android.view.View
-import android.widget.Button
-import android.widget.EditText
-import android.widget.ListView
-import android.widget.ProgressBar
+import android.view.inputmethod.InputMethodManager
+import android.widget.*
 import com.github.bassaer.qiitaclient.model.ArticleClient
 import com.trello.rxlifecycle2.components.support.RxAppCompatActivity
 import com.trello.rxlifecycle2.kotlin.bindToLifecycle
@@ -27,6 +26,7 @@ class MainActivity : RxAppCompatActivity() {
         val searchButton = findViewById<Button>(R.id.search_button)
         val progressBar = findViewById<ProgressBar>(R.id.progress_bar)
         val listView = findViewById<ListView>(R.id.list_view)
+        val mainLayout = findViewById<LinearLayout>(R.id.main_layout)
 
         val listAdapter = ArticleListAdapter(applicationContext)
 
@@ -36,10 +36,14 @@ class MainActivity : RxAppCompatActivity() {
             ArticleActivity.intnet(this, article).let { startActivity(it) }
         }
 
-
+        val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
 
         searchButton.setOnClickListener {
             progressBar.visibility = View.VISIBLE
+            inputMethodManager.hideSoftInputFromWindow(
+                    mainLayout.windowToken, InputMethodManager.HIDE_NOT_ALWAYS
+            )
+            mainLayout.requestFocus()
             articleClient.search(queryEditText.text.toString())
                     .subscribeOn(Schedulers.io())
                     .observeOn(AndroidSchedulers.mainThread())
